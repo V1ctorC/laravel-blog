637512fcbd3b36343fbc150602aa8953251252f6
Victor Chevalier <chevalier.victor@yahoo.fr>
2022-01-11 20:57:46 +0100

    Include CSS and JavaScript + Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My first posts</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet. Est adipisci libero et quia ullam non omnis magnam et suscipit quod sit quia quibusdam. Et fuga alias cum cumque galisum sit sint adipisci.
+
+            Aut explicabo nemo est dolorem Quis At fuga autem sit rerum libero qui fugiat dicta! Ea cumque odit rem minus asperiores aut reiciendis praesentium. Sed porro galisum ut vitae doloremque eos quia harum est rerum iure.
+
+            Ut iure quia eos sapiente earum ut alias sint est aliquid velit aut consequuntur alias aut asperiores voluptatem. At rerum modi quo voluptas corporis sed molestias laudantium qui voluptatibus dolores aut officiis alias!
+        </p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
