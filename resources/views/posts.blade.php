d59d521acac583cdfd23d92ee7fbf79802050e05
Victor Chevalier <chevalier.victor@yahoo.fr>
2022-01-13 20:24:57 +0100

    Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,39 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My first post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet. Est adipisci libero et quia ullam non omnis magnam et suscipit quod sit quia quibusdam. Et fuga alias cum cumque galisum sit sint adipisci.
-
-            Aut explicabo nemo est dolorem Quis At fuga autem sit rerum libero qui fugiat dicta! Ea cumque odit rem minus asperiores aut reiciendis praesentium. Sed porro galisum ut vitae doloremque eos quia harum est rerum iure.
-
-            Ut iure quia eos sapiente earum ut alias sint est aliquid velit aut consequuntur alias aut asperiores voluptatem. At rerum modi quo voluptas corporis sed molestias laudantium qui voluptatibus dolores aut officiis alias!
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My second post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet. Est adipisci libero et quia ullam non omnis magnam et suscipit quod sit quia quibusdam. Et fuga alias cum cumque galisum sit sint adipisci.
-
-            Aut explicabo nemo est dolorem Quis At fuga autem sit rerum libero qui fugiat dicta! Ea cumque odit rem minus asperiores aut reiciendis praesentium. Sed porro galisum ut vitae doloremque eos quia harum est rerum iure.
-
-            Ut iure quia eos sapiente earum ut alias sint est aliquid velit aut consequuntur alias aut asperiores voluptatem. At rerum modi quo voluptas corporis sed molestias laudantium qui voluptatibus dolores aut officiis alias!
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My third post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet. Est adipisci libero et quia ullam non omnis magnam et suscipit quod sit quia quibusdam. Et fuga alias cum cumque galisum sit sint adipisci.
-
-            Aut explicabo nemo est dolorem Quis At fuga autem sit rerum libero qui fugiat dicta! Ea cumque odit rem minus asperiores aut reiciendis praesentium. Sed porro galisum ut vitae doloremque eos quia harum est rerum iure.
-
-            Ut iure quia eos sapiente earum ut alias sint est aliquid velit aut consequuntur alias aut asperiores voluptatem. At rerum modi quo voluptas corporis sed molestias laudantium qui voluptatibus dolores aut officiis alias!
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post ?>
+        </article>
+    <?php endforeach; ?>
 </body>
