2850c53ecafd3cbb64a17b54a68c4c3d579ebae9
V1ctorC <chevalier.victor@yahoo.fr>
2022-02-07 19:27:27 +0100

    Quick Tweaks and Clean-Up

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -27,10 +27,8 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-2">
-                <p>
-                    {{ $post->excerpt }}
-                </p>
+            <div class="text-sm mt-2 space-y-4">
+                {!! $post->excerpt !!}
 
                 <p class="mt-4">
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
