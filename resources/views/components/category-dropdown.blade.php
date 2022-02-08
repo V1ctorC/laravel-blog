d2d735ff1b45f00e8e5ba1cd6d4f90442eb0988d
V1ctorC <chevalier.victor@yahoo.fr>
2022-02-08 20:02:00 +0100

    Extract a Category Dropdown Blade Component

---
+++ resources/views/components/category-dropdown.blade.php
@@ -0,0 +1,19 @@
+<x-dropdown>
+    <x-slot name="trigger">
+        <button @click.away="show = false" class="py-2 pl-3 pr-9 text-sm w-full lg:w-32 font-semibold inline-flex">
+            {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+
+            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px" />
+        </button>
+    </x-slot>
+
+    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+
+    @foreach($categories as $category)
+        <x-dropdown-item
+            href="/?category={{ $category->slug }}"
+            :active="isset($currentCategory) && $currentCategory->id === $category->id"
+        >{{ ucwords($category->name) }}
+        </x-dropdown-item>
+    @endforeach
+</x-dropdown>
