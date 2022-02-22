efd318856fb1d9b534c31788f9c200477f99a37f
V1ctorC <chevalier.victor@yahoo.fr>
2022-02-22 17:28:10 +0100

    Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 p-6 border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at consequatur
+            doloribus ducimus earum eius eligendi exercitationem expedita, iusto nihil quo
+            recusandae sapiente sint temporibus tenetur ullam voluptas voluptate voluptatem!</p>
+    </div>
+</article>
