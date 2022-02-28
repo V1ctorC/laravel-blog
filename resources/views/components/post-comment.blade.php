11aa747cfb0e2acf1cedf6991e8ea24df58201cd
V1ctorC <chevalier.victor@yahoo.fr>
2022-02-28 20:13:26 +0100

    Design the Comment Form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,19 +1,21 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 p-6 border-gray-200 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">
-                Posted
-                <time>{{ $comment->created_at }}</time>
-            </p>
-        </header>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at }}</time>
+                </p>
+            </header>
 
-        {{ $comment->body }}
-    </div>
-</article>
+            {{ $comment->body }}
+        </div>
+    </article>
+</x-panel>
