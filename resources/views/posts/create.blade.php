7bb986edd8aebc25d0734d44d0237cf051d3889e
V1ctorC <chevalier.victor@yahoo.fr>
2022-03-11 18:34:47 +0100

    Validate and Store Post Thumbnails

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,12 @@
 <x-layout>
     <x-slot name="content">
-        <section class="px-6 py-8">
+        <section class="px-6 py-8 max-w-md mx-auto">
+            <h1 class="text-lg font-bold mb-4">
+                Publish new post
+            </h1>
+
             <x-panel class="max-w-sm mx-auto">
-                <form action="/admin/posts" method="POST">
+                <form action="/admin/posts" method="POST" enctype="multipart/form-data">
                     @csrf
 
                     <div class="mb-6">
@@ -37,6 +41,24 @@
                         >
 
                         @error('slug')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="mb-6">
+                        <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                            slug
+                        </label>
+
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="file"
+                               name="thumbnail"
+                               id="thumbnail"
+                               value="{{ old('thumbnail') }}"
+                               required
+                        >
+
+                        @error('thumbnail')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
